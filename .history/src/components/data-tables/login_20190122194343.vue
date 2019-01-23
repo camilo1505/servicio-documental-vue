be7ae31b43a98031af26cacb2091868040ce1a64
Cristian Camilo Giraldo <cristian_camilo1505@hotmail.com>
2019-01-22 21:08:58 -0500

    login

---
+++ .history/src/components/data-tables/login_20190122194343.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-layout>
+                <v-flex xs12 md4>
+                    <v-text-field v-model="user" label="Usuario" required></v-text-field>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
+</template>
---
+++ .history/src/components/data-tables/login_20190122194756.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-layout>
+                <v-flex xs12 md4>
+                    <v-text-field v-model="user" label="Usuario" required></v-text-field>
+                </v-flex>
+                <v-flex xs12 md4>
+                    <v-text-field v-model="password" label="Contraseña" required :type="'password'"></v-text-field>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+import Axios from 'axios';
+export default {
+    name:'Login',
+    data() {
+        return {
+            user: null,
+            password: null,
+            respuesta: null
+        }
+    },
+}
+</script>
