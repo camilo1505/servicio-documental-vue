11f12b6922fd71859951673c449bb1ec9542c8bc
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-03-27 17:59:48 -0500

    cambios

--- src/components/documentos/preview.vue
+++ src/components/documentos/preview.vue
@@ -1,21 +1,60 @@
 <template>
-  <pdf src="http://localhost:8080/resources/misterySnow/japones/08_Compiladores_.pdf" :page=1></pdf>
-</template>
+  <div class="text-xs-center">
+    <v-dialog
+      v-model="dialog"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-on="on"
+        >
+          visualizar
+        </v-btn>
+      </template>
 
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          {{nombre}}
+        </v-card-title>
+
+        <v-card-text>
+        <iframe :src="url" width="1200" height="800">
+          <p>No se puede mostrar el archivo</p>
+        </iframe>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            Cerrar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+ 
 <script>
-import axios from 'axios'
-export default {
-  components: {
-  },
-  mounted(){
-    const board = 'resources/misterySnow/japones/08_Compiladores_.pdf';
-    var config = {
-        headers: {'Access-Control-Allow-Origin': '*'}
-    };
-    axios.get('http://localhost:8080' + board + '/threads.json', config)
-        .then(function (response) {
-            console.log(response.data);
-    });
+  export default {
+    props: {
+        url:null,
+        nombre:null
+    },
+    data () {
+      return {
+        archivo:"",
+        dialog: false
+      }
+    }
   }
-}
 </script>
